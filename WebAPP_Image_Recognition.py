a2200acebf8162f140be9f864e84b527e38f7800
Erlinda Gilberta Wibawa <85277959+erlindagilbertaw@users.noreply.github.com>
2026-04-03 02:52:50 +0800

    Uncomment code and update model loading method

--- WebAPP_Image_Recognition.py
+++ WebAPP_Image_Recognition.py
@@ -17,14 +17,16 @@
 #st.sidebar.markdown("Information")
 #st.sidebar.button("Press")
 
+
+#D:\PROJECT\MachineLearning\WebAPP_Image_Recognition
 import streamlit as st
-#Write in TERMINAL: Streamlit run Streamlit.py
+#Write in TERMINAL: streamlit run "D:\PROJECT\MachineLearning\WebAPP_Image_Recognition\WebAPP_Image_Recognition.py"
 import tensorflow as tf
 import numpy as np
 
 #Tensorflow Model Prediction
 def model_prediction(test_image):
-    model = tf.keras.models.load_model("trained_model.h5")
+    model = tf.keras.models.load_model("trained_model.keras")
     image = tf.keras.preprocessing.image.load_img(test_image,target_size=(64,64))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr]) #convert single image to batch
